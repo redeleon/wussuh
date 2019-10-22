7087a0002084ad9571bdcf4935df5a25f4fef122
Rafael de Leon <rafael.leon@mnltechnology.com>
2019-10-22 22:37:04 +0800

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,42 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-     {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(s*)css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
             }
-          }
         ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ]
-  },
-  devServer: {
-    contentBase: './dist'
-  }
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx'
+        ]
+    },
+    devServer: {
+        contentBase: './dist'
+    }
 }
 
 module.exports = config;
